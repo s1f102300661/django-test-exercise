b2844fdbd9074fdf92f3d974c934dc8cb4ec4722
1F10230274 <s1f102302742@iniad.org>
2024-07-19 10:02:14 +0900

    写真添付機能の追加

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from todo import views as todo_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +28,4 @@ urlpatterns = [
     path('<int:task_id>/update', todo_views.update, name='update'),
     path('<int:task_id>/close', todo_views.close, name='close'),
     path('toggle_favorite/<int:task_id>/', todo_views.toggle_favorite, name='toggle_favorite'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- todo/views.py
+++ todo/views.py
@@ -3,13 +3,14 @@ from django.http import Http404
 from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
 from todo.models import Task
+from django.conf import settings
 
 # Create your views here.
 
 
 def index(request):
     if request.method == 'POST':
-        task = Task(title=request.POST['title'], due_at=make_aware(parse_datetime(request.POST['due_at'])))
+        task = Task(title=request.POST['title'], due_at=make_aware(parse_datetime(request.POST['due_at'])), photo=request.FILES.get('photo'))
         task.save()
 
     if request.GET.get('order') == 'due':
